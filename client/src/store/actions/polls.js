b3bc25578951641cbf560a898a812eea92983be3
Your New Name <Your new email>
2019-03-16 11:33:11 +0200

    added actions and reducers for polls

---
+++ client/src/store/actions/polls.js
@@ -0,0 +1,78 @@
+import api from '../../services/api';
+import { SET_POLLS, SET_CURRENT_POLL } from "../actionTypes";
+import { addError, removeError } from "./error";
+
+export const setPolls = polls => ({
+   type: SET_POLLS,
+   polls
+});
+
+export const setCurrentPool = poll => ({
+   type: SET_CURRENT_POLL,
+   poll
+});
+
+export const getPools = () => {
+  return async dispatch => {
+      try {
+          const polls = await api.call('get','polls');
+          dispatch(setPolls(polls));
+          dispatch(removeError());
+      } catch (err) {
+          const error = err.response.data;
+          dispatch(addError(error));
+      }
+  }
+};
+
+export const getUserPolls = () => {
+    return async dispatch => {
+        try {
+            const polls = await api.call('get','polls/user');
+            dispatch(setPolls(polls));
+            dispatch(removeError());
+        } catch (err) {
+            const error = err.response.data;
+            dispatch(addError(error));
+        }
+    }
+};
+
+export const createPoll = data => {
+  return async dispatch => {
+      try {
+          const poll = await api.call('post', 'polls', data);
+          dispatch(setCurrentPool(poll));
+          dispatch(removeError());
+      } catch (err) {
+          const error = err.response.data;
+          dispatch(addError(error));
+      }
+  }
+};
+
+export const getCurrentPoll = path => {
+  return async dispatch => {
+      try {
+          const poll = await api.call('get', `polls/${path}`);
+          dispatch(setCurrentPool(poll));
+          dispatch(removeError());
+      } catch (err) {
+          const error = err.response.data;
+          dispatch(addError(error));
+      }
+  }
+};
+
+export const vote = (path, data) => {
+  return async dispatch => {
+      try {
+          const poll = await api.call('post', `polls/${path}`, data);
+          dispatch(setCurrentPool(poll));
+          dispatch(removeError());
+      } catch (err) {
+          const error = err.response.data;
+          dispatch(addError(error));
+      }
+  }
+};
